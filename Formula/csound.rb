4687f5b62d97633123a665e04d5013ce03f737a2
Steven Yi <stevenyi@gmail.com>
2014-05-14 12:27:00 -0400

    updated for 6.03.2

--- Formula/csound.rb
+++ Formula/csound.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Csound < Formula
   homepage 'http://csound.github.io'
-  url 'http://sourceforge.net/projects/csound/files/csound6/Csound6.03/Csound6.03.1.tar.gz/download'
-  sha1 '01c1f4c9c346b1ce28bc30cc0cd3dcccf0b278b3'
+  url 'http://sourceforge.net/projects/csound/files/csound6/Csound6.03/Csound6.03.2.tar.gz/download'
+  sha1 '2ff8081c887ef98735197c1ff70bb4955c3eb3ff'
   head 'https://github.com/csound/csound.git', :branch => 'develop'
-  version '6.03.1'
+  version '6.03.2'
 
   depends_on 'cmake' => :build
   depends_on 'swig' => :build
